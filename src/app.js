aa0c5179979dea77b4d5cd797a2fb5705a630669
Alan David Barcos <alandbarcos@gmail.com>
2023-11-01 22:38:16 -0300

    ProgramacionBackend-Coderhouse-Desafio04

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
 import express from "express";
 import productsRouter from './routes/products.router.js'
 import cartsRouter from './routes/carts.router.js'
+import productsViews from './routes/views.router.js'
+import handlebars from 'express-handlebars';
+import {Server} from 'socket.io';
+import __dirname from './utils.js';
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
 app.use('/api/products',productsRouter)
 app.use('/api/carts',cartsRouter)
+app.use('/', productsViews);
 
-app.listen(8080, () => console.log("Listening... 🏃‍♂️"))
+const httpServer=app.listen(8080, ()=> console.log("Listening... 🏃‍♂️"));
+const io= new Server(httpServer);
+
+io.on('connection', async socket =>{
+
+    console.log("Nuevo usuario conectado");
+});
+
+export {io}
